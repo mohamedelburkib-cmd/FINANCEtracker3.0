fd3baa5d34bda57a3c4a391e18b648c5887e356a
mohamedelburkib-cmd <mohamedelburkib@gmail.com>
2025-10-30 19:18:54 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+console.log("[main] mounting…");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
